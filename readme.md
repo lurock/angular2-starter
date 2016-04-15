eb024f9feaf8489b1c58b6c990c508f47d05a876
Jami Lurock <Jami.Lurock@SoCreate.it>
2016-04-14 23:14:01 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Getting Started
+
+To get all modules:
+
+    npm install
+
+To run:  
+
+    npm start
---
+++ src/app/app.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'app',
+    template: '<h1>Angular 2  Starter</h1>'
+})
+export class AppComponent { }
